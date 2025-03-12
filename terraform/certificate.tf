fe8e739dbacfac18949466a8e52e2db3ec96ff8f
Jovan <jovanrlr@gmail.com>
2025-03-11 20:17:44 -0700

    Create and validate certificate

--- terraform/certificate.tf
+++ terraform/certificate.tf
@@ -1,5 +1,7 @@
 resource "aws_acm_certificate" "certificate_request" {
+  provider = aws.east
   domain_name = var.domain_name
+  subject_alternative_names = [ var.subdomain_name ]
   validation_method = "DNS"
 
 
@@ -9,8 +11,8 @@ resource "aws_acm_certificate" "certificate_request" {
 }
 
 
-
 resource "aws_acm_certificate_validation" "certificate_validation" {
+  provider = aws.east
   certificate_arn         = aws_acm_certificate.certificate_request.arn
   validation_record_fqdns = [for record in aws_route53_record.validation_record : record.fqdn]
 }
