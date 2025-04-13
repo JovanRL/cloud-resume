247aeb9000ef136fa1a5cc0c97b8b508f8c2cd07
Jovan <jovanrlr@gmail.com>
2025-04-12 17:47:06 -0700

    Create a DynamoDB database

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,21 @@
+resource "aws_dynamodb_table" "visitsDB" {
+  name             = "visits"
+  hash_key         = "userID"
+  billing_mode     = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "userID"
+    type = "S"
+  }
+
+  attribute {
+    name = "visitDate"
+    type = "S"
+  }
+
+    global_secondary_index {
+    name               = "visitDate"
+    hash_key           = "visitDate"
+    projection_type    = "ALL"
+  }
+}
