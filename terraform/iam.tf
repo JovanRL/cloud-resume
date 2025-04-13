807c60ad18ee64e3f097ee0980830f741ee9dbeb
Jovan <jovanrlr@gmail.com>
2025-04-12 18:13:15 -0700

    Create role and policy for Lambda

---
+++ terraform/iam.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_role" "lambda_dynamo_role" {
+  name = "LambdaDynamoRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_dynamo_policy" {
+  name = "lambda_dynamo_policy"
+  role = aws_iam_role.lambda_dynamo_role.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1428341300017",
+            "Action": [
+                "dynamodb:DeleteItem",
+                "dynamodb:GetItem",
+                "dynamodb:PutItem",
+                "dynamodb:Query",
+                "dynamodb:Scan",
+                "dynamodb:UpdateItem"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Sid": "",
+            "Resource": "*",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Effect": "Allow"
+        }
+    ]
+})
+}
