36103d73fcdd3f644f41d1104f8de2d2ad5ddaa9
Jovan <jovanrlr@gmail.com>
2025-04-17 22:26:25 -0700

    Add permission to invoke Lambda from APIGateway

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,11 +5,20 @@ data "archive_file" "lambda" {
 }
 
 resource "aws_lambda_function" "go_lambda_counter" {
-
+  provider = aws.east
   function_name = "go_lambda_counter"
   role          = aws_iam_role.lambda_dynamo_role.arn
   handler       = "main.handler"
   filename       = "lambda_function.zip"
 
   runtime       = "provided.al2"
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  provider = aws.east
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.go_lambda_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.visits_api.execution_arn}/*/*"
 }
