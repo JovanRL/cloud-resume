d29b154745963dca930cd055eb78d527021d6099
Jovan <jovanrlr@gmail.com>
2025-04-20 21:28:28 -0700

    Fetch the API to get visits counter

--- frontend/script.js
+++ frontend/script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+const visits = document.getElementsByClassName("visits")[0]
+
+async function getData() {
+  const url = "https://api.jovan.cloud/visits";
+
+  try {
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    
+    const data = await response.json();
+
+    visits.innerHTML = `This page has been visited ${data} times`
+
+  } catch (error) {
+    console.error(error.message);
+    visits.innerHTML = `This page has been visited, at least, 5 times`
+  }
+}
+
+getData()
